a4d01bc4428d1a4abfba4cb0ae79708b7a9c29b6
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-14 17:05:00 -0500

    fill in boilerplate for routers

---
+++ routes/api/index.js
@@ -0,0 +1,13 @@
+/// tools ///
+const express = require ('express')
+
+/***************************************
+  MAIN
+***************************************/
+
+const router = express.Router ()
+
+/**************************************/
+
+/// exports ///
+module.exports = router

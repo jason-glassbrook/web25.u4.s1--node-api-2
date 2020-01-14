dff246ab295f09bb8960728434ff343ab7be0f82
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-14 17:33:14 -0500

    add dummy GET responses for / and /api

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,6 +7,12 @@ const express = require ('express')
 
 const router = express.Router ()
 
+router.get ('/', (ri, ro) => {
+  ro
+    .status (200)
+    .send (`hello! i'm the api.`)
+})
+
 /**************************************/
 
 /// exports ///
